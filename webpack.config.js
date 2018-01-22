1bf4201da378858399199f80eed30f4f3f6fc35d
John Guo <john.guo@sortable.com>
2018-01-22 15:56:18 -0500

    Setup build with webpack and refactor project structure.

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const webpack = require("webpack");
+
+var PATHS = {
+  entryPoint: path.resolve(__dirname, 'src/ads.ts'),
+  bundles: path.resolve(__dirname, '_bundles')
+};
+
+module.exports = {
+  entry: {
+    'ads-manager': [PATHS.entryPoint],
+    'ads-manager.min': [PATHS.entryPoint]
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    port:9000
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      sourceMap: true,
+      include: /\.min\.js$/,
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        enforce: 'pre',
+        loader: 'tslint-loader'
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            compilerOptions: {
+              declaration: false
+            }
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    path: PATHS.bundles,
+    filename: '[name].js',
+    libraryTarget: 'umd',
+    library: 'AdsManager',
+    umdNamedDefine: true
+  }
+}
