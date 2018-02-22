919cb9d3210377a3f183eb29ef2b6622b44a358d
John Guo <john.guo@sortable.com>
2018-02-22 15:35:47 -0500

    Fix for test.

--- src/types.d.ts
+++ src/types.d.ts
@@ -77,8 +77,8 @@ declare namespace SortableAds {
   interface Setting {
     bidderTimeout: number;
     throttleTimeout: number;
-    reactVersion: string;
-    adsReactVersion: string;
+    reactVersion?: string;
+    adsReactVersion?: string;
   }
 
   interface UpdateEvent<T, K extends keyof T> {
